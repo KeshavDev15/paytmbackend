bea5e089966bed386bdc5dfee08897d201991e10
Keshav Agarwal <agarwalkeshav875@gmail.com>
2025-02-05 10:57:16 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 // backend/db.js
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://admin:Admin@cluster0.kciya.mongodb.net/paytm');
+mongoose.connect(connectionURL);
 // Create a Schema for Users
 const userSchema = new mongoose.Schema({
     username: {
